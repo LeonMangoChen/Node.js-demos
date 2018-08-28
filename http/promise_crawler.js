0917efbe6e1b7618acde11c34f840597e5aa01a5
LeonMangoChen <leonmangochen@163.com>
2018-08-28 16:09:20 +0800

    Revise promise_crawler.js

--- http/promise_crawler.js
+++ http/promise_crawler.js
@@ -2,15 +2,16 @@ var http = require('https');
 var Promise = require('bluebird');
 var cheerio = require('cheerio');
 var baseUrl = 'https://www.imooc.com/learn/';
+var learnNumber_baseUrl = 'https://www.imooc.com/course/AjaxCourseMembers?ids=';
 var url = 'https://www.imooc.com/learn/348';
 var videoIds = [348, 259];
 
-function fiterChapters(html) {
+function filerChapters(pageData) {
+    var html = pageData.html;
     var $ = cheerio.load(html);
     var chapters = $('.chapter');
     var title = $('.hd .l').text();
-    var number = parseInt($('.js-learn-num').text().trim(), 10);
-
+    var number = pageData.number;
     // courseData = {
     //     title: title,
     //     number: number,
@@ -24,8 +25,9 @@ function fiterChapters(html) {
     // }
 
     var courseData = {
+        title: title,
+        number: number,
         videos: []
-
     }
 
     chapters.each(function(item) {
@@ -52,20 +54,18 @@ function fiterChapters(html) {
 }
 
 function printCourseInfo(coursesData) {
-    coursesData.forEach(function(argument) {
-        console.log(coursesData.number + ' 人学过' + coursesData.title + '\n');
+    coursesData.forEach(function(courseData) {
+        console.log(courseData.number + ' 人学过 ' + courseData.title + '\n');
     })
 
-
     coursesData.forEach(function(courseData) {
-        console.log('### ' + courseData.title + '\n')
+        console.log('### ' + courseData.title + '\n');
         courseData.videos.forEach(function(item) {
-            console.log('### ' + courseData.title + '\n')
             var chapterTitle = item.chapterTitle;
             console.log(chapterTitle + '\n');
 
             item.videos.forEach(function(video) {
-                console.log(' 【' + video.id + '】' + video.title + '\n');
+                console.log(' 【' + video.id + '】' + video.title.trim().split('(')[0]);
             })
         })
     })
@@ -73,7 +73,7 @@ function printCourseInfo(coursesData) {
 
 function getPageAsync(url) {
     return new Promise(function(resolve, reject) {
-        console.log('正在爬去' + url)
+        console.log('正在爬取' + url)
         http.get(url, function(res) {
             var html = '';
 
@@ -82,11 +82,7 @@ function getPageAsync(url) {
             });
 
             res.on('end', function() {
-                // console.log(html)
-                // var courseData = fiterChapters(html);
                 resolve(html);
-
-                // printCourseInfo(courseData);
             })
         }).on('error', function(e) {
             reject(e);
@@ -96,10 +92,41 @@ function getPageAsync(url) {
     })
 }
 
+function getLearnDataAsync(html) {
+    return new Promise(function(resolve, reject) {
+        var $ = cheerio.load(html);
+        var id = $('.person-num').attr('href').split('/')[2];
+        var pageData = {
+            html: html,
+            number: 0
+        };
+
+        var db = '';
+        http.get(learnNumber_baseUrl + id, function(res) {
+            res.on('data', function(data) {
+                db += data;
+                db = JSON.parse(db);
+                pageData.number = parseInt(db.data[0].numbers, 10);
+            });
+
+            res.on('end', function() {
+                resolve(pageData);
+            });
+
+        }).on('error', function() {
+            console.log('error');
+        });
+    });
+}
+
+
+
 var fetchCourseArray = [];
 
 videoIds.forEach(function(id) {
-    fetchCourseArray.push(getPageAsync(baseUrl + id));
+    fetchCourseArray.push(getPageAsync(baseUrl + id).then(function(html) {
+        return getLearnDataAsync(html);
+    }));
 })
 
 Promise
@@ -107,8 +134,8 @@ Promise
     .then(function(pages) {
         var coursesData = [];
 
-        pages.forEach(function(html) {
-            var courses = fiterChapters(html);
+        pages.forEach(function(pageData) {
+            var courses = filerChapters(pageData);
             coursesData.push(courses);
         })
 
